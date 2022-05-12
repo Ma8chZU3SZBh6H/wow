9bb185464b5f837ddb6b6ac0b6ab547bb81f41c0
Ma8chZU3SZBh6H <38836685+Ma8chZU3SZBh6H@users.noreply.github.com>
2022-05-12 17:44:26 +0300

    home page partner section added

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,13 @@ import type {NextPage} from 'next'
 import Nav from "../components/Nav";
 import Socials from "../components/Socials";
 
+const partners = [
+    '/home/partners/fast.svg',
+    '/home/partners/forbes.svg',
+    '/home/partners/insider.svg',
+    '/home/partners/tnw.svg',
+    '/home/partners/wired.svg',
+]
 
 const Home: NextPage = () => {
     return (
@@ -51,6 +58,14 @@ const Home: NextPage = () => {
                         src='/home/header/app.png' alt=''/>
                 </div>
                 <img className='z-20 absolute bottom-0 right-0 md:w-auto w-1/2' src='/home/header/cubes.svg' alt=''/>
+            </section>
+            <section className='grid grid-rows-2 items-center justify-center lg:py-20 md:py-16 p-6 text-center gap-6'>
+                <h5 className='text-blue-dust text-12px font-extrabold'>OUR PARTNERS</h5>
+                <div className='flex justify-center items-center gap-16 flex-wrap'>
+                    {partners.map((url, index) => <img className='h-[21.01px] md:h-[23.34px] lg:h-[38.13px]'
+                                                       key={index + 'aab'}
+                                                       src={url} alt=''/>)}
+                </div>
             </section>
         </main>
     )
